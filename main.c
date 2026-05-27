53a28b29d192ae087f6daeeb582654b56e6c0ca7
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-05-27 09:57:53 -0300

    Adicionada função histórico() e vetores para tentativas consecutivas

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int OP;
-char comu;
-float temp, bater; 
+int OP, total;
+char comu[10];
+float temp[10], bater[10]; 
 
 
 void menu (){
@@ -16,7 +16,8 @@ void menu (){
     printf("1 - Inserir informacoes\n");
     printf("2 - Visualizar status\n");
     printf("3 - Executar analise\n");
-    printf("4 - Encerrar sistema\n");
+    printf("4 - Historico de dados\n");
+    printf("5 - Encerrar sistema\n");
     scanf("%d", &OP);
 }
 
@@ -27,43 +28,44 @@ void dados (){
     printf("--------------------------\n");
 
     printf("Qual a temperatura atual?\n");
-    scanf("%f", &temp);
+    scanf("%f", &temp[total]);
 
-    if(temp > 80){
+    if(temp[total] > 80){
         printf("=========================\n");
         printf("ALERTA: SUPERAQUECIMENTO\n");
         printf("=========================\n");
         printf("Aguarde o resfriamento do sistema.\n");
-    }else if (temp <= 10){
+    }else if (temp[total] <= 10){
         printf("=========================\n");
         printf("ALERTA: CONGELAMENTO\n");
         printf("=========================\n");
     }
 
     printf("Qual o nivel de bateria atual? \n");
-    scanf("%f", &bater);
+    scanf("%f", &bater[total]);
 
-    while (bater <= 0){
-        printf("Valor de bateria invalido, por favor responda com seriedade");
-        printf("Qual o nivel de bateria atual? ");
-        scanf("%f", &bater);
+    while (bater[total] <= 0){
+        printf("Valor de bateria invalido, por favor responda com seriedade\n");
+        printf("Qual o nivel de bateria atual?\n ");
+        scanf("%f", &bater[total]);
     }
-    if(bater < 20 && bater > 0){
+    if(bater[total] < 20 && bater[total] > 0){
         printf("Entrando em modo de economia de energia\n");
     }
 
     printf("Sua comunicacao esta ativa?\n S - Sim\n N - Nao\n");
-    scanf(" %c", &comu);
+    scanf(" %c", &comu[total]);
 
-    while(comu != 'S' && comu != 's' && comu != 'n' && comu != 'N' ){
+    while(comu[total] != 'S' && comu[total] != 's' && comu[total] != 'n' && comu[total] != 'N' ){
         printf("Resposta invalida, por favor responda com seriedade\n");
         printf("Sua comunicacao esta ativa?\n S - Sim\n N - Nao\n");
-        scanf(" %c", &comu);
+        scanf(" %c", &comu[total]);
     }
 
-    if(comu == 'N' || comu == 'n'){
+    if(comu[total] == 'N' || comu[total] == 'n'){
         printf("Falha de comunicacao, reinicie o sistema.\n");
     } 
+    total++;
 }
 
 void visu_dados () {
@@ -72,10 +74,9 @@ void visu_dados () {
     printf("|      VISUALIZACAO DOS DADOS       |\n");
     printf("-------------------------------------\n");
 
-
-    printf("Sua temperatura atual e: %.2f graus celsius\n", temp);
-    printf("Sua bateria atual e: %.2f %%\n", bater);
-    if(comu == 's' || comu == 'S'){
+    printf("Sua temperatura atual e: %.2f graus celsius\n", temp[total-1]);
+    printf("Sua bateria atual e: %.2f %%\n", bater[total-1]);
+    if(comu[total-1] == 's' || comu[total-1] == 'S'){
         printf("Ha comunicacao disponivel no momento atual: SIM\n");
     }else {
         printf("Ha comunicacao disponivel no momento atual: NAO\n");
@@ -88,11 +89,11 @@ void analise (){
     printf("|         ANALISE        |\n");
     printf("--------------------------\n");
 
-    if(temp > 80){
+    if(temp[total-1] > 80){
         printf("A temperatura do sistema esta maior que o esperado\n");
         printf("Por favor, aguarde o resfriamento do sistema\n");
         printf("Temperatura: CRITICO\n");
-    }else if(temp <= 10){
+    }else if(temp[total-1] <= 10){
         printf("A temperatura do sistema esta menor que o esperado\n");
         printf("Por favor, aguarde o aquecimento do sistema\n");
         printf("Temperatura: CRITICO\n");        
@@ -102,10 +103,10 @@ void analise (){
 
     printf("");
 
-    if(bater<20 && bater>10){
+    if(bater[total-1]<20 && bater[total-1]>10){
         printf("Recomendamos que você continue no modo economia de energia\n");
         printf("Bateria: RUIM\n");
-    }else if (bater<10){
+    }else if (bater[total-1]<10){
         printf("Carregue o sistema IMEDIATAMENTE\n");
         printf("Bateria: CRITICO\n");
     }else{
@@ -114,12 +115,35 @@ void analise (){
 
     printf("");
 
-    if(comu == 'N' || comu == 'n'){
+    if(comu[total-1] == 'N' || comu[total-1] == 'n'){
         printf("Ha uma falha na comunicacao com a central de informacoes\n");
         printf("Por favor, reinicie o sistema manualmente para reestabeler a comunicacao com a central\n");
         printf("Comunicacao: CRITICO\n");
     }else{
-        printf("Cominicacao: OK\n");
+        printf("Comunicacao: OK\n");
+    }
+}
+
+void historico (){
+    printf("---------------------------------------------\n");
+    printf("|          HISTORICO DE LEITURAS            |\n");
+    printf("---------------------------------------------\n");
+ 
+    if (total == 0) {
+        printf("Nenhuma leitura registrada ainda.\n");
+        return;
+    }
+ 
+    printf("%-8s %-14s %-12s %-14s\n",
+           "Leitura", "Temp (C)", "Bateria (%)", "Comunicacao");
+    printf("---------------------------------------------\n");
+ 
+    for (int i = 0; i < total; i++) {
+        printf("  #%-6d %-14.2f %-12.2f %s\n",
+               i + 1,
+               temp[i],
+               bater[i],
+               (comu[i] == 'S' || comu[i] == 's') ? "ATIVA" : "FALHA");
     }
 }
 
@@ -146,12 +170,17 @@ int main (){
                 system("cls");
                 break;
             case 4:
+                historico();
+                system("pause");
+                system("cls");
+                break;
+            case 5:
                 break;
             default:
                 printf("opcao indisponivel, tente novamente\n");
         }
 
-    }while (OP != 4); 
+    }while (OP != 5); 
     printf("Encerrando sistema!");
     printf("Tenha uma otima missao\n");
     system("pause");
