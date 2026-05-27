f43cd2e16c8a7b3d37f068950bca1b1ba78b6468
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-05-27 09:21:55 -0300

    Esqueleto final pronto, com funções dados(), visu_dados() e analise() corrigidas e finalizadas

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int OP, comu;
+int OP;
+char comu;
 float temp, bater; 
 
 
-int menu (){
+void menu (){
     
     printf("======================================\n");
     printf("|           MISSAO ESPACIAL          |\n");
     printf("======================================\n");
 
     printf("Digite a opcao desejada:\n");
-    printf("1 - inserir informacoes\n");
-    printf("2 - visualizar status\n");
-    printf("3 - executar analise\n");
-    printf("4 - encerrar sistema\n");
+    printf("1 - Inserir informacoes\n");
+    printf("2 - Visualizar status\n");
+    printf("3 - Executar analise\n");
+    printf("4 - Encerrar sistema\n");
     scanf("%d", &OP);
 }
 
-int dados (){
+void dados (){
+
+    printf("--------------------------\n");
+    printf("|      NOVOS DADOS       |\n");
+    printf("--------------------------\n");
+
     printf("Qual a temperatura atual?\n");
     scanf("%f", &temp);
 
@@ -27,12 +34,21 @@ int dados (){
         printf("ALERTA: SUPERAQUECIMENTO\n");
         printf("=========================\n");
         printf("Aguarde o resfriamento do sistema.\n");
+    }else if (temp <= 10){
+        printf("=========================\n");
+        printf("ALERTA: CONGELAMENTO\n");
+        printf("=========================\n");
     }
 
-    printf("Qual o nivel de bateria atual? ");
+    printf("Qual o nivel de bateria atual? \n");
     scanf("%f", &bater);
 
-    if(bater < 20){
+    while (bater <= 0){
+        printf("Valor de bateria invalido, por favor responda com seriedade");
+        printf("Qual o nivel de bateria atual? ");
+        scanf("%f", &bater);
+    }
+    if(bater < 20 && bater > 0){
         printf("Entrando em modo de economia de energia\n");
     }
 
@@ -48,9 +64,64 @@ int dados (){
     if(comu == 'N' || comu == 'n'){
         printf("Falha de comunicacao, reinicie o sistema.\n");
     } 
-
 }
 
+void visu_dados () {
+
+    printf("-------------------------------------\n");
+    printf("|      VISUALIZACAO DOS DADOS       |\n");
+    printf("-------------------------------------\n");
+
+
+    printf("Sua temperatura atual e: %.2f graus celsius\n", temp);
+    printf("Sua bateria atual e: %.2f %%\n", bater);
+    if(comu == 's' || comu == 'S'){
+        printf("Ha comunicacao disponivel no momento atual: SIM\n");
+    }else {
+        printf("Ha comunicacao disponivel no momento atual: NAO\n");
+    }
+}
+
+void analise (){
+
+    printf("--------------------------\n");
+    printf("|         ANALISE        |\n");
+    printf("--------------------------\n");
+
+    if(temp > 80){
+        printf("A temperatura do sistema esta maior que o esperado\n");
+        printf("Por favor, aguarde o resfriamento do sistema\n");
+        printf("Temperatura: CRITICO\n");
+    }else if(temp <= 10){
+        printf("A temperatura do sistema esta menor que o esperado\n");
+        printf("Por favor, aguarde o aquecimento do sistema\n");
+        printf("Temperatura: CRITICO\n");        
+    }else{
+        printf("Temperatura: OK\n");
+    }
+
+    printf("");
+
+    if(bater<20 && bater>10){
+        printf("Recomendamos que você continue no modo economia de energia\n");
+        printf("Bateria: RUIM\n");
+    }else if (bater<10){
+        printf("Carregue o sistema IMEDIATAMENTE\n");
+        printf("Bateria: CRITICO\n");
+    }else{
+        printf("Bateria: OK\n");
+    }
+
+    printf("");
+
+    if(comu == 'N' || comu == 'n'){
+        printf("Ha uma falha na comunicacao com a central de informacoes\n");
+        printf("Por favor, reinicie o sistema manualmente para reestabeler a comunicacao com a central\n");
+        printf("Comunicacao: CRITICO\n");
+    }else{
+        printf("Cominicacao: OK\n");
+    }
+}
 
 int main (){
     do{
@@ -59,10 +130,20 @@ int main (){
         switch(OP){
             case 1:
                 dados ();
+                system("pause");
+                system("cls");
                 break;
+
             case 2:
+                visu_dados ();
+                system("pause");
+                system("cls");
                 break;
+
             case 3:
+                analise ();
+                system("pause");
+                system("cls");
                 break;
             case 4:
                 break;
@@ -72,7 +153,9 @@ int main (){
 
     }while (OP != 4); 
     printf("Encerrando sistema!");
-    printf("Tenha uma otima missao");
+    printf("Tenha uma otima missao\n");
+    system("pause");
+    system("cls");
 
 return 0;
 }
